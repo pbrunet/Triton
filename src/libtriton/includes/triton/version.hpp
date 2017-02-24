91b9cd7b1d018545efb0c0f18cd087a27f6abf23
pbrunet <pbrunet@quarkslab.com>
2017-02-24 12:32:19 +0100

    WIP

---
+++ src/libtriton/includes/triton/version.hpp
@@ -0,0 +1,32 @@
+//! \file
+/*
+**  Copyright (C) - Triton
+**
+**  This program is under the terms of the BSD License.
+*/
+
+#ifndef TRITON_VERSION_H
+#define TRITON_VERSION_H
+
+
+
+//! The Triton namespace
+namespace triton {
+/*!
+ *  \addtogroup triton
+ *  @{
+ */
+
+  //! The libTriton's version.
+  enum version_e {
+    MAJOR = 0,
+    MINOR = 4,
+    BUILD = 1353
+
+  };
+
+/*! @} End of triton namespace */
+};
+
+#endif // TRITON_VERSION_H
+
